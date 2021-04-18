9ede511483b2da1e0a54911afa66b408bfc945a2
santiago <santiagosantose101@gmail.com>
2021-04-17 23:54:01 -0500

    VERSION 1:Laboratorio JPA-1

--- src/main/java/com/example/models/Competitor.java
+++ src/main/java/com/example/models/Competitor.java
@@ -3,16 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.example.models;
+import com.sun.istack.NotNull;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Set;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 
 /**
  *
  * @author Mauricio
  */
-public class Competitor{
-     
+@Entity
+public class Competitor implements Serializable{
     
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    @Column(name = "create_at", updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar createdAt;
+    @NotNull
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.DATE)
+    private Calendar updatedAt;
+
     private String name;
     
     private String surname;
@@ -48,77 +77,82 @@ public class Competitor{
         winner=winnerN;
     }
     
-   
-    public String getName() {
-        return name;
-    }
+            @OneToMany(cascade=ALL, mappedBy="competitor")
+         private Set<Producto> products;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+         public Set<Producto> getProducts() {
+          return products;
+         }
 
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public String getCellphone() {
-        return cellphone;
-    }
-
-    public void setCellphone(String cellphone) {
-        this.cellphone = cellphone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public boolean isWinner() {
-        return winner;
-    }
-
-    public void setWinner(boolean winner) {
-        this.winner = winner;
-    }
-    
+  @PreUpdate
+ private void updateTimestamp() {
+ this.updatedAt = Calendar.getInstance();
+ }
+ @PrePersist
+ private void creationTimestamp() {
+ this.createdAt = this.updatedAt = Calendar.getInstance();
+ }
+ public Long getId() {
+ return id;
+ }
+ public void setId(Long id) {
+ this.id = id;
+ }
+ public String getName() {
+ return name;
+ }
+ public void setName(String name) {
+ this.name = name;
+ }
+ public String getSurname() {
+ return surname;
+ }
+ public void setSurname(String surname) {
+ this.surname = surname;
+ }
+ public int getAge() {
+ return age;
+ }
+ public void setAge(int age) {
+ this.age = age;
+ }
+ public String getTelephone() {
+ return telephone;
+ }
+ public void setTelephone(String telephone) {
+ this.telephone = telephone;
+ }
+ public String getCellphone() {
+ return cellphone;
+ }
+ public void setCellphone(String cellphone) {
+ this.cellphone = cellphone;
+ }
+ public String getAddress() {
+ return address;
+ }
+ public void setAddress(String address) {
+ this.address = address;
+ }
+ public String getCity() {
+ return city;
+ }
+ public void setCity(String city) {
+ this.city = city;
+ }
+ public String getCountry() {
+ return country;
+ }
+ public void setCountry(String country) {
+ this.country = country;
+ }
+ public boolean isWinner() {
+ return winner;
+ }
+ public void setWinner(boolean winner) {
+ this.winner = winner;
+ }
 }
+
+    
+
--- src/main/java/com/example/services/CompetitorService.java
+++ src/main/java/com/example/services/CompetitorService.java
@@ -5,46 +5,89 @@
  */
 package com.example.services;
 
+import com.example.PersistenceManager;
 import com.example.models.Competitor;
 import com.example.models.CompetitorDTO;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.codehaus.jettison.json.JSONObject;
 
 /**
  *
  * @author Mauricio
  */
+
 @Path("/competitors")
 @Produces(MediaType.APPLICATION_JSON)
 public class CompetitorService {
 
+@PersistenceContext(unitName = "CompetitorsPU")
+ EntityManager entityManager;
+    
+    @PostConstruct
+ public void init() {
+    try {
+    entityManager =
+   PersistenceManager.getInstance().getEntityManagerFactory().createEntityManager();
+    } catch (Exception e) {
+    e.printStackTrace();
+    }
+    }
+ 
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll() {
 
-        
-        List<Competitor> competitors = new ArrayList<Competitor>();
-        Competitor competitorTmp= new Competitor("Carlos", "Alvarez", 35, "7658463", "3206574839 ", "carlos.alvarez@gmail.com", "Bogota", "Colombia", false);
-        Competitor competitorTmp2= new Competitor("Gustavo", "Ruiz", 55, "2435231", "3101325467", "gustavo.ruiz@gmail.com", "Buenos Aires", "Argentina", false);
-        competitors.add(competitorTmp);
-        competitors.add(competitorTmp2);
-        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitors).build();
+        Query q = entityManager.createQuery("select u from Competitor u order by u.surname ASC");
+        List<Competitor> competitors = q.getResultList();
+        return Response.status(200).header("Access-Control-Allow-Origin","*").entity(competitors).build();
+
     }
+
 
     @POST
     @Path("/add")
     @Produces(MediaType.APPLICATION_JSON)
     public Response createCompetitor(CompetitorDTO competitor) {
-
-        Competitor competitorTmp= new Competitor(competitor.getName(), competitor.getSurname(), competitor.getAge(), competitor.getTelephone(), competitor.getCellphone(), competitor.getAddress(), competitor.getCity(), competitor.getCountry(), false);
-        return Response.status(200).header("Access-Control-Allow-Origin", "*").entity(competitorTmp).build();
-    }
-
+ JSONObject rta = new JSONObject();
+ Competitor competitorTmp = new Competitor();
+ competitorTmp.setAddress(competitor.getAddress());
+ competitorTmp.setAge(competitor.getAge());
+ competitorTmp.setCellphone(competitor.getCellphone());
+ competitorTmp.setCity(competitor.getCity());
+ competitorTmp.setCountry(competitor.getCountry());
+ competitorTmp.setName(competitor.getName());
+ competitorTmp.setSurname(competitor.getSurname());
+ competitorTmp.setTelephone(competitor.getTelephone());
+ try {
+ entityManager.getTransaction().begin();
+ entityManager.persist(competitorTmp);
+ entityManager.getTransaction().commit();
+ entityManager.refresh(competitorTmp);
+ rta.put("competitor_id", competitorTmp.getId());
+ } catch (Throwable t) {
+ t.printStackTrace();
+ if (entityManager.getTransaction().isActive()) {
+ entityManager.getTransaction().rollback();
+ }
+ competitorTmp = null;
+ } finally {
+ entityManager.clear();
+ entityManager.close();
+ }
+ return Response.status(200).header("Access-Control-Allow-Origin","*").entity(rta).build();
+    
+    
+}
 }
